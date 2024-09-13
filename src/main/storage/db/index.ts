f2495ef9b0f069c307a7c3b921ba097553a1811a
Eandalf <clinah@connect.ust.hk>
2024-09-13 23:54:33 +0800

    chore: move data dir to ./app_data

--- src/main/storage/db/index.ts
+++ src/main/storage/db/index.ts
@@ -7,7 +7,7 @@ import { PlayList } from "@main/storage/playlist/playlist.entity";
 import { Table } from "./table.enum";
 import { TableDefault } from "./table.default";
 
-const dataDir = app.getPath("userData");
+const dataDir = path.join(app.getPath("userData"), "./app_data");
 
 /**
  * Write data to disk by table name.
