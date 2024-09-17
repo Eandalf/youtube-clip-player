591e87b8086c1ca03a624fa5f9ecbc425eaf0a13
Eandalf <clinah@connect.ust.hk>
2024-09-17 15:54:01 +0800

    test: set up jest

---
+++ jest.config.ts
@@ -0,0 +1,24 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  roots: ["<rootDir>/src/tests/unit"],
+  moduleNameMapper: {
+    "^@app/(.*)$": "<rootDir>/src/app/$1",
+    "^@main/(.*)$": "<rootDir>/src/main/$1",
+  },
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.(t|j)sx?$": [
+      "ts-jest",
+      { tsconfig: "tsconfig.json" },
+    ],
+  },
+  collectCoverage: true,
+  coverageDirectory: "<rootDir>/.jest/coverage",
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.(t|j)sx?',
+  ],
+  cacheDirectory: "<rootDir>/.jest/cache",
+};
+
+export default config;
---
+++ src/tests/unit/__tests__/storage.ts
@@ -0,0 +1,9 @@
+import { describe, test, expect } from "@jest/globals";
+
+describe("Storage", () => {
+  test("equal", () => {
+    const test1 = 1;
+    const test2 = 1;
+    expect(test1).toBe(test2);
+  });
+});
