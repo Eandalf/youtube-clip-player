2f73117879b89bbc608c4cca458908de31648dc9
Eandalf <clinah@connect.ust.hk>
2024-09-23 22:27:33 +0800

    feat: nav link open & close

--- src/app/components/Dashboard/NavLink/index.tsx
+++ src/app/components/Dashboard/NavLink/index.tsx
@@ -1,9 +1,20 @@
 import { type PropsWithChildren } from "react";
+import { NavLink as RouterNavLink } from "react-router-dom";
 
-const NavLink = ({ children }: PropsWithChildren) => {
-  return (<div className="m-8">
-    {children}
-  </div>);
+interface NavLinkProp extends PropsWithChildren {
+  to: string;
+}
+
+const NavLink = ({ to, children }: NavLinkProp) => {
+  return (
+    <RouterNavLink to={to}>
+      {({ isActive }) => (
+        <div className={`px-8 py-4 hover:shadow-xl${isActive && " bg-blue-400"}`}>
+          {children}
+        </div>
+      )}
+    </RouterNavLink>
+  );
 };
 
 export default NavLink;
--- src/app/components/Dashboard/index.tsx
+++ src/app/components/Dashboard/index.tsx
@@ -1,23 +1,45 @@
+import {
+  useState,
+  MouseEvent,
+} from "react";
 import { Outlet } from "react-router-dom";
 import { ScrollArea } from "@app/components/ui/scroll-area";
 import { Separator } from "@app/components/ui/separator";
 import NavLink from "./NavLink";
 
 const Dashboard = () => {
+  const [openNavLinks, setOpenNavLinks] = useState(true);
+
+  const onNavOpen = (e: MouseEvent<HTMLElement>) => {
+    e.preventDefault();
+    setOpenNavLinks((nav) => !nav);
+  };
+
   return (
     <div className="container p-0 h-full flex flex-row flex-nowrap justify-start items-stretch">
-      <ScrollArea className="flex-initial">
-        <NavLink>Link 1</NavLink>
-        <Separator className="my-4" />
-        <NavLink>Link 2</NavLink>
-        <Separator className="my-4" />
-        <NavLink>Link 3</NavLink>
-        <Separator className="my-4" />
-        <NavLink>Link 4</NavLink>
-        <Separator className="my-4" />
-        <NavLink>Link 5</NavLink>
+      <ScrollArea className={`flex-initial shadow hover:shadow-lg transition-all${!openNavLinks && " w-10"}`}>
+        <div className="h-10 flex flex-row flex-nowrap justify-end">
+          <div
+            className="flex-initial w-10 h-10 p-2 border-solid border-2 border-grey-400 rounded flex flex-col justify-between"
+            onClick={onNavOpen}
+          >
+            <span className={`flex-initial w-6 h-1 border-r-2 bg-slate-800 transition-all${openNavLinks && " origin-center rotate-45 translate-y-[9px]"}`} />
+            <span className={`flex-initial w-6 h-1 border-r-2 bg-slate-800 transition-all${openNavLinks && " origin-center hidden"}`} />
+            <span className={`flex-initial w-6 h-1 border-r-2 bg-slate-800 transition-all${openNavLinks && " origin-center -rotate-45 -translate-y-[9px]"}`} />
+          </div>
+        </div>
+        {openNavLinks && (<>
+          <NavLink to="/">Home</NavLink>
+          <Separator />
+          <NavLink to="/test">Link 2</NavLink>
+          <Separator />
+          <NavLink to="/test">Link 3</NavLink>
+          <Separator />
+          <NavLink to="/test">Link 4</NavLink>
+          <Separator />
+          <NavLink to="/test">Link 5</NavLink>
+        </>)}
       </ScrollArea>
-      <Separator orientation="vertical" className="flex-initial mr-4" />
       <div className="flex-auto p-8">
         <Outlet />
       </div>
